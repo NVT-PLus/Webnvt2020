8e6d9856173b68413979bb060da177c1fd076670
NVT-PLus <71747080+NVT-PLus@users.noreply.github.com>
2020-11-06 00:22:02 +0700

    Update config_DB.js

--- configs/config_DB.js
+++ configs/config_DB.js
@@ -1,13 +1,13 @@
 module.exports = {
     localdb: {
-        urldb : 'mongodb+srv://nvt:XdFZtf7p5u8wON2d@atnshop.h3ufw.mongodb.net/atnshop?retryWrites=true&w=majority',
+        urldb : 'mongodb+srv://NVTSHOP:nvt@atnshop.h3ufw.mongodb.net/atnshop?retryWrites=true&w=majority',
         dbname : "atnshop",
         dbusername : "",
         dbpassword : "",
     },
 
     clouddb: {
-        urldb : 'mongodb+srv://nvt:XdFZtf7p5u8wON2d@atnshop.h3ufw.mongodb.net/atnshop?retryWrites=true&w=majority',
+        urldb : 'mongodb+srv://NVTSHOP:nvt@atnshop.h3ufw.mongodb.net/atnshop?retryWrites=true&w=majority',
         dbname : "atnshop",
         dbusername : "",
         dbpassword : "",
